146a5d97442a20b94bb2b3f0c186b77a32577020
jssalazar <jssalazar3@espe.edu.ec>
2020-07-26 00:19:02 -0500

    SITIO MODULE

---
+++ src/app/cementerio/sitio/sitio.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SitiosComponent } from './sitios/sitios.component';
+import { SitioDetallesComponent } from './sitio-detalles/sitio-detalles.component';
+import { SitioService } from './service/sitio.service';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AngularModule } from 'src/app/angular.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+
+
+@NgModule({
+  declarations: [
+    SitiosComponent,
+    SitioDetallesComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    CommonModule,
+    AngularModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
+  ],
+  exports: [
+    AngularModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  providers:[
+    SitioService
+  ]
+})
+export class SitioModule { }
