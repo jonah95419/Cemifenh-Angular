1f7716e235f8edf787461ebfb68772a4a322857f
jssalazar <jssalazar3@espe.edu.ec>
2020-07-27 18:36:44 -0500

    INICIO MODULE

---
+++ src/app/inicio/inicio.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InicioComponent } from './inicio/inicio.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { AngularModule } from '../angular.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { HistorialComponent } from './historial/historial.component';
+
+@NgModule({
+  declarations: [InicioComponent, HistorialComponent],
+  imports: [
+    AppRoutingModule,
+    CommonModule,
+    AngularModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
+  ],
+  exports: [
+    AngularModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  providers:[
+  ]
+})
+
+export class InicioModule { }
