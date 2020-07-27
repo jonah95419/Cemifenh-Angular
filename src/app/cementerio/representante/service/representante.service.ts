120ced5887f76dfab40490786323c94f18ff00e0
jssalazar <jssalazar3@espe.edu.ec>
2020-07-27 18:35:48 -0500

    REPRESENTANTE SERVICE

--- src/app/cementerio/representante/service/representante.service.ts
+++ src/app/cementerio/representante/service/representante.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError, BehaviorSubject } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { RepresentanteResponse, RepresentanteI } from '../model/representante';
 
 const AUTH_SERVER = environment.baseUrl;
 
@@ -13,52 +14,77 @@ export class RepresentanteService {
 
   constructor(private httpClient: HttpClient) { }
 
-  agregarRepresentante(representante: RepresentanteI): Observable<any> {
-    return this.httpClient.post<any>(`${AUTH_SERVER}/representante/`, JSON.stringify(representante), this.httpOptions)
-    .pipe(catchError(this.handleError));
+  private _representantes = new BehaviorSubject<RepresentanteI[]>([]);
+  private dataStore: { representantes: RepresentanteI[] } = { representantes: [] };
+  readonly representantes = this._representantes.asObservable();
+
+  listarRepresentantes = (): void => {
+    this.httpClient
+    .get<RepresentanteResponse>(`${AUTH_SERVER}/representante/`, this.httpOptions)
+    .pipe( catchError(this.handleError) )
+    .subscribe((data: RepresentanteResponse) => {
+      if(data.ok) {
+        this.dataStore.representantes = data.data;
+        this._representantes.next(Object.assign({}, this.dataStore).representantes);
+      } else {
+        console.log("mostar mensaje de error");
+      }
+    });
   }
 
-  obtenerEstadoCuentaRepresentante(representanteId: number): Observable<any> {
-    return this.httpClient.get<any>(`${AUTH_SERVER}/representante/estado-cuenta/${representanteId}`, this.httpOptions)
-    .pipe(
-      catchError(this.handleError)
-    );
+  listarRepresentantesPeriodo = (desde: string, hasta: string): void => {
+    this.httpClient
+    .get<RepresentanteResponse>(`${AUTH_SERVER}/representante/periodo/${desde}&${hasta}`, this.httpOptions)
+    .pipe( catchError(this.handleError) )
+    .subscribe((data: RepresentanteResponse) => {
+      if(data.ok) {
+        this.dataStore.representantes = data.data;
+        this._representantes.next(Object.assign({}, this.dataStore).representantes);
+      } else {
+        console.log("mostar mensaje de error");
+      }
+    });
   }
+
+  obtenerEstadoCuentaRepresentante(representanteId: string): Observable<any> {
+    return this.httpClient
+    .get<any>(`${AUTH_SERVER}/representante/estado-cuenta/${representanteId}`, this.httpOptions)
+    .pipe( catchError(this.handleError) );
+  }
+
+
+
+
+
+
 
   obtenerDeudasRepresentante(representanteId: number): Observable<any> {
     return this.httpClient.get<any>(`${AUTH_SERVER}/representante/deudas/${representanteId}`, this.httpOptions)
-    .pipe(
-      catchError(this.handleError)
-    );
+    .pipe( catchError(this.handleError) );
   }
 
   obtenerPagoDetallesRepresentante(comprobanteID: number): Observable<any> {
     return this.httpClient.get<any>(`${AUTH_SERVER}/representante/pago-detalles/${comprobanteID}`, this.httpOptions)
-    .pipe(
-      catchError(this.handleError)
-    );
+    .pipe( catchError(this.handleError) );
   }
 
   obtenerPagosRepresentante(representanteId: number): Observable<any> {
     return this.httpClient.get<any>(`${AUTH_SERVER}/representante/pagos/${representanteId}`, this.httpOptions)
-    .pipe(
-      catchError(this.handleError)
-    );
-  }
-
-  listarRepresentantes(): Observable<any> {
-    return this.httpClient.get<any>(`${AUTH_SERVER}/representante/`, this.httpOptions)
-    .pipe(
-      catchError(this.handleError)
-    );
+    .pipe( catchError(this.handleError) );
   }
 
   obtenerRepresentante(representanteId: number): Observable<any> {
     return this.httpClient.get<any>(`${AUTH_SERVER}/representante/${representanteId}`, this.httpOptions)
-    .pipe(
-      catchError(this.handleError)
-    );
+    .pipe( catchError(this.handleError) );
   }
+
+
+  agregarRepresentante(representante: RepresentanteI): Observable<any> {
+    return this.httpClient
+    .post<any>(`${AUTH_SERVER}/representante/`, JSON.stringify(representante), this.httpOptions)
+    .pipe( catchError(this.handleError) );
+  }
+
 
   private handleError(error: HttpErrorResponse) {
     return throwError(
@@ -71,9 +97,4 @@ export class RepresentanteService {
       // 'authorization': `Bearer ${JSON.parse(localStorage.getItem('user')).stsTokenManager.accessToken}`
     })
   };
-}
-
-interface RepresentanteI {
-  representante: string;
-  cedula: string;
 }
