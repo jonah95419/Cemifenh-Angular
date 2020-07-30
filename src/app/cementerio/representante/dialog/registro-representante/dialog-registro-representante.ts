7708114a66e29f4d8c16cc0af22ce48b1887f009
jssalazar <jssalazar3@espe.edu.ec>
2020-07-30 13:24:42 -0500

    REPRESENTANTE DIALOG REGISTRO

--- src/app/cementerio/representante/dialog/registro-representante/dialog-registro-representante.ts
+++ src/app/cementerio/representante/dialog/registro-representante/dialog-registro-representante.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { SectorService } from '../../../../admin/service/sector.service';
 import { MAT_DATE_LOCALE, DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
 import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
 import { SectorI } from '../../../../admin/model/sector';
 import { Validators, FormControl, FormBuilder } from '@angular/forms';
-import { SitioI } from '../../../sitio/sitio-detalles/sitio-detalles.component';
 import { tap } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Importacion } from '../../../../utilidades/importarRegistros';
 import { ValoresService } from '../../../../admin/service/valores.service';
 import { ValorI } from '../../../../admin/model/valor';
-import { combineLatest } from 'rxjs';
+import { RepresentanteService } from '../../service/representante.service';
+import { RegistroI } from '../../../../utilidades/model/registro';
+import { MatSelectChange } from '@angular/material/select';
+import { MatCheckboxChange } from '@angular/material/checkbox';
+import { DeudaI } from '../../model/deuda';
+import { SitioI } from '../../../sitio/sitio-detalles/sitio-detalles.component';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { MatTable } from '@angular/material/table';
 
 export interface DialogData {
   animal: string;
@@ -32,21 +38,29 @@ export interface DialogData {
     { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
   ],
 })
-export class DialogRegistroRepresentante {
+export class DialogRegistroRepresentante implements OnInit, OnDestroy {
+
+  @ViewChild(MatTable) table: MatTable<any>;
+
 
   step = 0;
 
+  columnsToDisplay: string[] = ['descripcion', 'desde', 'hasta', 'valor', 'accion'];
+
   registrarSitio: boolean = false;
   registrarFallecido: boolean = false;
+  generarDeudas: boolean = false;
 
-  listaTipo = ['Arriendo', 'Compra', 'Donación'];
-  listaDescripcion = ['Bóveda', 'Lote propio', 'Piso'];
+  listaTipo = ['Arriendo', 'Propio', 'Donación'];
+  listaDescripcion = ['Boveda', 'Piso propio', 'Piso'];
   listaSectores: SectorI[];
   listaValores: ValorI[];
+  listaDeudas: DeudaI[];
 
   representanteForm = this.fb.group({
     nombre: new FormControl('', Validators.required),
-    cedula: new FormControl('')
+    cedula: new FormControl(''),
+    observaciones: ''
   })
 
   sitioForm = this.fb.group({
@@ -55,55 +69,78 @@ export class DialogRegistroRepresentante {
     tipo: new FormControl('', Validators.required),
     descripcion: new FormControl('', Validators.required),
     fecha: new FormControl('', Validators.required),
+    observaciones: ""
   })
 
   fallecidoForm = this.fb.group({
     nombre: new FormControl('', Validators.required),
     cedula: new FormControl(''),
-    fecha_fallecimiento: new FormControl('')
+    fecha_fallecimiento: new FormControl(''),
+    observaciones: ''
   })
 
   sectorControl = new FormControl('', Validators.required);
 
+  locale: string;
+
   informacionSitio: SitioI;
 
+  private _translate;
+
   constructor(
+    private translate: TranslateService,
     private _snackBar: MatSnackBar,
     private fb: FormBuilder,
     private apiSectores: SectorService,
     private apiValores: ValoresService,
+    private apiRepresentante: RepresentanteService,
     private dialogRef: MatDialogRef<DialogRegistroRepresentante>) {
     apiValores.valores.pipe(tap((data: ValorI[]) => this.listaValores = data)).toPromise();
     apiSectores.sectores.pipe(tap((data: SectorI[]) => this.listaSectores = data)).toPromise();
   }
 
+  ngOnInit() {
+    this.locale = this.translate.currentLang;
+    this._translate = this.translate.onLangChange
+      .subscribe((langChangeEvent: LangChangeEvent) => { this.locale = langChangeEvent.lang; })
+
+  }
+
+  ngOnDestroy(): void {
+    if (this._translate !== undefined) {
+      this._translate.unsubscribe();
+    }
+  }
+
   submit(): void {
-    let nuevo_representante = {
+    let nuevo_registro = {
       representante: {},
       sitio: null,
       fallecido: null
     }
     if (this.representanteForm.valid) {
-      nuevo_representante.representante = this.representanteForm.value;
+      nuevo_registro.representante = this.representanteForm.value;
       if (this.registrarSitio) {
         if (this.sitioForm.valid) {
-          nuevo_representante.sitio = this.sitioForm.value;
+          nuevo_registro.sitio = this.sitioForm.value;
           if (this.registrarFallecido) {
             if (this.fallecidoForm.valid) {
-              nuevo_representante.fallecido = this.fallecidoForm.value;
-              this.guardarRegistro(nuevo_representante);
+              nuevo_registro.fallecido = this.fallecidoForm.value;
+              this.guardarRegistro(nuevo_registro);
             } else {
               this.openSnackBar("Faltan campos de llenar del fallecido", "");
             }
           } else {
-            this.guardarRegistro(nuevo_representante);
+            this.guardarRegistro(nuevo_registro);
           }
         } else {
           this.openSnackBar("Faltan campos de llenar del sitio", "");
         }
       } else {
-        this.guardarRegistro(nuevo_representante);
+        this.guardarRegistro(nuevo_registro);
       }
+    } else {
+      this.openSnackBar("Faltan campos de llenar del represenante", "");
     }
   }
 
@@ -115,10 +152,98 @@ export class DialogRegistroRepresentante {
 
   prevStep() { this.step--; }
 
-  private guardarRegistro(registro: object): void {
-    console.log(registro);
-    console.log(this.listaValores);
+  generarDeudasSitios(event: MatCheckboxChange) {
+    if(event.checked) {
+      this.generarListaDeudas(this.sitioForm.value);
+    } else {
+      this.listaDeudas = [];
+    }
+  }
+
+  tipoSeleccionado(event: MatSelectChange) {
+    const value = event.source.value;
+    if(value === "Arriendo") {
+      this.listaDescripcion = ['Boveda', 'Piso'];
+    }
+    if(value === "Propio") {
+      this.listaDescripcion = ['Piso propio'];
+    }
+    if(value === "Donación" ) {
+      this.listaDescripcion = ['Boveda', 'Piso'];
+    }
+  }
+
+  getTotalCost() {
+    return this.listaDeudas?.map(t => Number(t.valor)).reduce((acc, value) => acc + value, 0);
+  }
+
+  eliminarDeuda(deuda: DeudaI) {
+    this.listaDeudas = this.listaDeudas.filter(d => d.valorId !== deuda.valorId);
+    this.table.renderRows();
+  }
+
+  private generarListaDeudas(r: any): void {
+    let nuevo_registro: RegistroI[] = [{
+      NombreRepresentante: "",
+      CedulaRepresentante: "",
+      ObservacionesRepresetante: "",
+      NombreFallecido: "",
+      CedulaFallecido: "",
+      FechaFallecimiento: null,
+      Observaciones: "",
+      Nombre: r.nombre,
+      Sector: r.sector,
+      Motivo: r.tipo,
+      Lugar: r.descripcion,
+      FechaAdquisicion: r.fecha,
+      FechaInicio: null,
+      ObservacionesSitio: "",
+      Total: "0"
+    }];
+
+    const importar:Importacion = new Importacion(this.listaSectores, this.listaValores);
+    this.listaDeudas = importar.generarDeudasRegistro(nuevo_registro);
+  }
+
+  private guardarRegistro(r: any): void {
+    let fallecido: boolean = r.fallecido === null;
+    let sitio: boolean = r.sitio !== null;
+    if (sitio) {
+      let nuevo_registro: any = {
+        representante: {
+          nombre: r.representante.nombre,
+          cedula: r.representante.cedula,
+          observaciones: r.representante.observaciones
+        },
+        sitio: {
+          nombre: r.sitio.nombre,
+          motivo: r.sitio.tipo,
+          lugar: r.sitio.descripcion,
+          sector: this.obtenerSector(r.sitio.sector),
+          fechaAdquisicion: r.sitio.fecha,
+          observaciones: r.sitio.observaciones
+        },
+        fallecido: {
+          nombre: r.fallecido?.nombre,
+          cedula: r.fallecido?.cedula,
+          fecha: r.fallecido?.fecha_fallecimiento,
+          observaciones: r.fallecido?.observaciones
+        },
+        deudas: this.listaDeudas
+      };
+      if(fallecido) {
+        nuevo_registro.fallecido = null;
+      }
+      this.apiRepresentante.agregarRepresentante(nuevo_registro);
+    } else {
+      this.apiRepresentante.agregarRepresentante(r);
+    }
+
     this.dialogRef.close();
+  }
+
+  private obtenerSector (id: any): SectorI {
+    return this.listaSectores.filter ( sector => sector.id == id)[0];
   }
 
   private openSnackBar = (message: string, action: string) => {
