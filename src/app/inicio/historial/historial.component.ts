1a289edf22d3954b9703b729b4fa4ad1ab8b5b1b
jssalazar <jssalazar3@espe.edu.ec>
2020-08-02 23:18:00 -0500

    INICIO HISTORIAL

--- src/app/inicio/historial/historial.component.ts
+++ src/app/inicio/historial/historial.component.ts
@@ -1,66 +1,129 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { RepresentanteService } from '../../cementerio/representante/service/representante.service';
 import { EstadoCuentaH } from '../../cementerio/representante/model/estadoCuentaR';
-import { tap } from 'rxjs/operators';
+import { tap, filter, map } from 'rxjs/operators';
+import { SitioService } from '../../cementerio/sitio/service/sitio.service';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogPagoExtra } from '../../cementerio/representante/dialog/registro-pago-extra/dialog-pago-extra';
+import { ResponseSitioI } from '../../cementerio/sitio/model/sitio';
 
 @Component({
   selector: 'app-historial',
   templateUrl: './historial.component.html',
   styleUrls: ['./historial.component.css']
 })
-export class HistorialComponent implements OnInit {
+export class HistorialComponent implements OnInit, OnDestroy {
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  displayedColumnsEC: string[] = [ 'descripcion', 'desde', 'hasta', 'cantidad', 'accion', 'eliminar'];
+  displayedColumnsEC: string[] = ['fecha', 'descripcion', 'desde', 'hasta', 'cantidad', 'accion', 'eliminar'];
 
   dataSourceEC: MatTableDataSource<EstadoCuentaH>;
 
   listaEstadoCuenta: EstadoCuentaH[] = [];
 
+  sitios: number = 0;
+
   id: string;
+  locale: string;
+
+  private _translate;
 
   constructor(
+    private translate: TranslateService,
     private apiRepresentante: RepresentanteService,
-    private route: ActivatedRoute ) {
-      route.paramMap.subscribe((data: ParamMap) => {
-        const representante = data.get("id");
-        this.obtenerHistorial(representante);
-      })
+    private apiSitio: SitioService,
+    private dialog: MatDialog,
+    private route: ActivatedRoute) {
+    route.paramMap.subscribe((data: ParamMap) => {
+      const representante = data.get("id");
+      this.obtenerHistorial(representante);
+    })
+  }
+
+  ngOnInit() {
+    this.locale = this.translate.currentLang;
+    this._translate = this.translate.onLangChange
+      .subscribe((langChangeEvent: LangChangeEvent) => { this.locale = langChangeEvent.lang; })
+
+  }
+
+  ngOnDestroy(): void {
+    if (this._translate !== undefined) {
+      this._translate.unsubscribe();
     }
-
-  ngOnInit(): void {
   }
 
-  getTotalEstadoCuenta() {
-    let suma = 0;
-    this.listaEstadoCuenta.forEach( t => { if ( new Date(t.desde) > new Date('2001/01/01') ) { if ( t.estado_cuenta === 'deuda' ) { suma += Number(t.cantidad); } else { suma -= Number(t.cantidad); } } });
+
+  agregarDeuda(): void {
+    window.alert("agregar deuda");
+  }
+
+  agregarPago(): void {
+    const dialogRef = this.dialog.open(DialogPagoExtra, {
+      width: '500px',
+      panelClass: "my-class",
+      data: { id: this.id }
+    });
+
+    dialogRef.afterClosed().subscribe();
+  }
+
+  getTotalEstadoCuenta(): number {
+    let suma: number = 0;
+    this.listaEstadoCuenta.forEach(t => {
+      if (new Date(t.fecha) > new Date('2001/01/01')) {
+        if (t.estado_cuenta === 'deuda') {
+          suma += Number(t.cantidad);
+        } else {
+          if(t.pago === "Servicio" || t.pago === "Mantenimiento") {
+            suma -= Number(t.cantidad);
+          }
+        }
+      }
+    });
     return suma;
   }
 
-  getDeudasEstadoCuenta() {
-    let suma = 0;
-    this.listaEstadoCuenta.forEach( t => { if ( t.estado_cuenta === 'deuda' && (new Date(t.desde) > new Date('2001/01/01'))) { suma += Number(t.cantidad); }});
+  getDeudasEstadoCuenta(): number {
+    let suma: number = 0;
+    this.listaEstadoCuenta.forEach(t => { if (t.estado_cuenta === 'deuda' && (new Date(t.fecha) > new Date('2001/01/01'))) { suma += Number(t.cantidad); } });
     return suma;
   }
 
-  getPagosEstadoCuenta() {
-    let suma = 0;
-    this.listaEstadoCuenta.forEach( t => { if ( t.estado_cuenta !== 'deuda' && (new Date(t.desde) > new Date('2001/01/01'))) { suma += Number(t.cantidad); }});
+  getPagosEstadoCuenta(): number {
+    let suma: number = 0;
+    this.listaEstadoCuenta.forEach(t => { if (t.estado_cuenta !== 'deuda' && (new Date(t.fecha) > new Date('2001/01/01'))) { suma += Number(t.cantidad); } });
     return suma;
   }
 
   private obtenerHistorial(id: string) {
     this.id = id;
     this.apiRepresentante.obtenerEstadoCuentaRepresentante(id).pipe(
-      tap( data => {
+      map(data => {
+        data.data = data.data.filter(d => d.cantidad !== null);
+        return data;
+      }),
+      tap(data => {
         if (data.ok) { this.cargarValoresEstadoCuenta(data.data); }
-        else { alert(data.message); }
+        else { console.log(data.message); }
+      })
+    ).toPromise();
+    this.cargarSitios(id);
+  }
+
+  private cargarSitios(id: string) {
+    this.sitios = 0;
+    this.apiSitio.listarSitios(id).pipe(
+      tap((data: ResponseSitioI) => {
+        if (data.ok) { this.sitios = data.cant; }
+        else { console.log(data.message); }
       })
     ).toPromise();
   }
