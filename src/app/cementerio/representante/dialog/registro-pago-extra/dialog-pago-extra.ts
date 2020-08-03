e9f0b145f959da2b127ba47504d3f770b311d6eb
jssalazar <jssalazar3@espe.edu.ec>
2020-08-02 23:20:37 -0500

    CEMENTERIO REPRESENTANTE DIALOG PAGO EXTRA

---
+++ src/app/cementerio/representante/dialog/registro-pago-extra/dialog-pago-extra.ts
@@ -0,0 +1,149 @@
+import { Component, OnInit, OnDestroy, Inject, ViewChild } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DATE_LOCALE, DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
+import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { SitioService } from '../../../sitio/service/sitio.service';
+import { tap } from 'rxjs/operators';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ResponseSitioI, SitioI, DeudaSitioI, ResponseDeudaSitioI } from '../../../sitio/model/sitio';
+import { MatSelectChange } from '@angular/material/select';
+import { MatCheckboxChange } from '@angular/material/checkbox';
+import { MatTable } from '@angular/material/table';
+
+@Component({
+  selector: 'dialog-pago-extra',
+  templateUrl: 'dialog-pago-extra.html',
+  styleUrls: ['./dialog-pago-extra.css'],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'es-EC' },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+  ],
+})
+export class DialogPagoExtra implements OnInit, OnDestroy {
+
+  @ViewChild(MatTable) table: MatTable<any>;
+
+  sitios: SitioI[] = [];
+  deudas: DeudaSitioI[] = [];
+  deudasAuxiliar: DeudaSitioI[] = [];
+  listaPagos: deudaComprobante[] = [];
+  columnsToDisplay: string[] = ['descripcion', 'cantidad', 'accion'];
+
+  locale: string;
+  otros: boolean = false;
+  sitio: SitioI;
+
+  pagoForm = this.fb.group({
+    deuda: new FormControl(''),
+    descripcion: new FormControl(''),
+    cantidad: new FormControl('', Validators.min(0)),
+  })
+
+  private _translate;
+
+  constructor(
+    private translate: TranslateService,
+    private apiSitio: SitioService,
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<DialogPagoExtra>,
+    @Inject(MAT_DIALOG_DATA) private data: any) {
+      this.cargarSitios(data.id);
+  }
+
+  onNoClick(): void { this.dialogRef.close(); }
+
+  ngOnInit() {
+    this.locale = this.translate.currentLang;
+    this._translate = this.translate.onLangChange
+      .subscribe((langChangeEvent: LangChangeEvent) => { this.locale = langChangeEvent.lang; })
+
+  }
+
+  ngOnDestroy(): void {
+    if (this._translate !== undefined) {
+      this._translate.unsubscribe();
+    }
+  }
+
+  submit (): void {
+    const value = this.pagoForm.value;
+    if(this.otros) {
+      if(value.descripcion !== "" && value.cantidad) {
+        this.listaPagos.push({
+          id:-1,
+          descripcion: value.descripcion,
+          cantidad: value.cantidad
+        });
+        this.table.renderRows();
+        this.pagoForm.reset();
+      }
+    } else {
+      if(value.deuda !== null && value.cantidad) {
+        this.listaPagos.push({
+          id: value.deuda.id,
+          descripcion: value.deuda.descripcion + " (" + value.deuda.desde + " - " + value.deuda.hasta + ")",
+          cantidad: value.cantidad
+        });
+        this.table.renderRows();
+        this.pagoForm.reset();
+      }
+    }
+  }
+
+  eliminarPago(row: deudaComprobante): void {
+    this.listaPagos = this.listaPagos.filter( d => d !== row);
+    this.table.renderRows();
+  }
+
+  getTotalCost() {
+    return this.listaPagos?.map(t => Number(t.cantidad)).reduce((acc, value) => acc + value, 0);
+  }
+
+  sitioSeleccionado(event: MatSelectChange): void {
+    const value = event.source.value;
+    if(value !== null) {
+      this.deudas = [];
+      this.deudasAuxiliar = [];
+      this.cargarDeudasSitio(value);
+    }
+  }
+
+  otroPago(event: MatCheckboxChange): void {
+    const value = event.checked;
+    this.otros = value;
+  }
+
+  private cargarSitios(id: string): void {
+    this.apiSitio.listarSitios(id).pipe(
+      tap( (data: ResponseSitioI) => {
+        if(data.ok) { this.sitios = data.data; }
+        else { console.log(data.message); }
+      })
+    ).toPromise();
+  }
+
+  private cargarDeudasSitio(sitio: SitioI): void {
+    this.sitio = sitio;
+    this.apiSitio.obtenerDeudas(sitio.id).pipe(
+      tap((data: ResponseDeudaSitioI) => {
+        if(data.ok) {
+          this.deudas = data.data;
+          this.deudasAuxiliar = this.deudas;
+        } else { console.log(data.message);}
+      })
+    ).toPromise();
+  }
+
+}
+
+interface deudaComprobante {
+  id: number;
+  descripcion: string;
+  cantidad: number;
+}
