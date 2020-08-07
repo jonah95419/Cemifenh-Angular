292a62eae16b75b6375194ff75fe852d76bf8b2e
jssalazar <jssalazar3@espe.edu.ec>
2020-08-07 16:39:55 -0500

    CEMENTERIO FALLECIDOS MODEL

---
+++ src/app/cementerio/fallecido/model/fallecido.ts
@@ -0,0 +1,27 @@
+export interface FallecidoI {
+  sitio: number;
+  nombre: string;
+  cedula: string;
+  fecha: Date;
+  observaciones: string;
+}
+
+export interface FallecidoRepresentanteI {
+  id: number;
+  nombre: string;
+  cedula: string;
+  fecha: Date;
+  observaciones: string;
+  sector: string;
+  sitio: string;
+  tipo: string;
+  descripcion: string;
+  adquisicion: Date;
+}
+
+export interface ResponseFallecidoRepresentanteI {
+  ok: boolean;
+  data?: FallecidoRepresentanteI[];
+  cant?: number;
+  message?: string;
+}
