f778ed539d9887e61e6f16fe1f94bb8704598e04
jssalazar <jssalazar3@espe.edu.ec>
2020-08-07 16:42:53 -0500

    CEMENTERIO SITIO SITIOS

--- src/app/cementerio/sitio/sitios/sitios.component.ts
+++ src/app/cementerio/sitio/sitios/sitios.component.ts
@@ -1,84 +1,61 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { ActivatedRoute, Params } from '@angular/router';
+import { tap } from 'rxjs/operators';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { SitioService } from '../service/sitio.service';
-
+import { ResponseSitioI, SitioI } from '../model/sitio';
 
 @Component({
   selector: 'app-sitios',
   templateUrl: './sitios.component.html',
   styleUrls: ['./sitios.component.css']
 })
-export class SitiosComponent implements OnInit, OnDestroy {
+export class SitiosComponent implements OnInit {
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  listaSitios = [];
+  listaSitios: SitioI[] = [];
   verDetalle = false;
-  idRepresentante: any;
   detalle: any;
   sitioId: number;
 
-  displayedColumns: string[] = ['id', 'nombre', 'sector', 'tipo', 'descripcion', 'estado', 'adquisicion', 'observaciones'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns: string[] = ['id', 'sector', 'tipo', 'descripcion', 'estado', 'adquisicion', 'observaciones'];
+  dataSource: MatTableDataSource<any>;
 
-  private state$: Observable<object>;
-  private subscribeSitios: any;
-
-  constructor(private activatedRoute: ActivatedRoute, private sitioservice: SitioService) { }
-
-  ngOnInit() {
-    this.state$ = this.activatedRoute.paramMap
-      .pipe(map(() => window.history.state));
-
-    this.state$.subscribe((data: any) => {
-      if (!data.id) {
-        const router = window.location.pathname;
-        this.idRepresentante = router.split('/')[2];
-      } else {
-        this.idRepresentante = data.id;
-      }
-      this.obtenerValores();
-    });
-
-  }
-
-  ngOnDestroy(): void {
-    if (this.subscribeSitios !== undefined) {
-      this.subscribeSitios.unsubscribe();
+  constructor(
+    private route: ActivatedRoute,
+    private apiSitios: SitioService) {
+      route.parent.params.pipe( tap((data: Params) => {
+        if(data.id) {
+          this.obtenerValores(data.id);
+        }
+      })).toPromise();
     }
-  }
 
-  verDetalles(sitio: any) {
+  ngOnInit() { }
+
+  verDetalles(sitio: any): void {
     this.verDetalle = true;
     this.sitioId = sitio.id;
   }
 
-  private obtenerValores() {
-    this.subscribeSitios = this.sitioservice.listarSitios(this.idRepresentante)
-    .subscribe(data => {
-      if (data.ok) { this.cargarValores(data); } else {alert(data.message); }
-    });
+  private obtenerValores(id: string): void {
+    this.apiSitios.listarSitios(id).pipe(
+      tap((data: ResponseSitioI) => {
+        if (data.ok) { this.cargarValores(data.data); } else { console.log(data.message); }
+      })
+    ).toPromise();
   }
 
-  private cargarValores(data) {
-    this.listaSitios = data.data;
+  private cargarValores(data: SitioI[]): void {
+    this.listaSitios = data;
     this.dataSource = new MatTableDataSource(this.listaSitios);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-    if (this.listaSitios.length <= 2) {this.verDetalles(this.listaSitios[0]); }
+    if (this.listaSitios.length != 0) {this.verDetalles(this.listaSitios[0]); }
   }
 
-}
-
-interface UserData {
-  id: string;
-  nombre: string;
-  cedula: string;
-  accion: string;
 }
