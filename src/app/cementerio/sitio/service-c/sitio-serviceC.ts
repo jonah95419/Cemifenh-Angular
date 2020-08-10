367d8dd76862c3af5865ad011188c0a7463a62dd
jssalazar <jssalazar3@espe.edu.ec>
2020-08-10 04:37:40 -0500

    CEMENTERIO SITIO SERVICE C (reestructuracion)

---
+++ src/app/cementerio/sitio/service-c/sitio-serviceC.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceC {
+
+  constructor() { }
+
+  private idSitioDetalle$ = new Subject<number>();
+  sitioDetalle$ = this.idSitioDetalle$.asObservable();
+  emitIdSitioDetalleChange(id_sitio: number) {this.idSitioDetalle$.next(id_sitio);}
+
+}
