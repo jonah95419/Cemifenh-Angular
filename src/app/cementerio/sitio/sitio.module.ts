e24c0187d5ba84b93f32542beb85d78c0852fe59
jssalazar <jssalazar3@espe.edu.ec>
2020-08-10 04:35:06 -0500

    CEMENTERIO SITIO MODULE (reestructuracion)

--- src/app/cementerio/sitio/sitio.module.ts
+++ src/app/cementerio/sitio/sitio.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SitiosComponent } from './sitios/sitios.component';
-import { SitioDetallesComponent } from './sitio-detalles/sitio-detalles.component';
 import { SitioService } from './service/sitio.service';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AngularModule } from 'src/app/angular.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { SitioDetallesInformacionComponent } from './sitio-detalles-informacion/sitio-detalles-informacion.component';
+import { SitioDetallesEstadoCuentaComponent } from './sitio-detalles-estado-cuenta/sitio-detalles-estado-cuenta.component';
+import { SitioDetallesListaFallecidosComponent } from './sitio-detalles-lista-fallecidos/sitio-detalles-lista-fallecidos.component';
 
 
 @NgModule({
   declarations: [
     SitiosComponent,
-    SitioDetallesComponent
+    SitioDetallesListaFallecidosComponent,
+    SitioDetallesInformacionComponent,
+    SitioDetallesEstadoCuentaComponent
   ],
   imports: [
     AppRoutingModule,
