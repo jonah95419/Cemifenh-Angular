8950796e167f1e819730106fa42c14bb46fb12d7
jssalazar <jssalazar3@espe.edu.ec>
2020-08-10 04:33:44 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,15 @@ import { ExportacionComponent } from './admin/exportacion/exportacion.component'
 import { CopiaSeguridadComponent } from './admin/copia-seguridad/copia-seguridad.component';
 import { RepresentanteInformacionComponent } from './cementerio/representante/representante-informacion/representante-informacion.component';
 import { SitiosComponent } from './cementerio/sitio/sitios/sitios.component';
-import { PagosComponent } from './cementerio/representante/pagos/pagos.component';
-import { DeudasComponent } from './cementerio/representante/deudas/deudas.component';
 import { EstadoCuentaComponent } from './cementerio/representante/estado-cuenta/estado-cuenta.component';
 import { FallecidosComponent } from './cementerio/fallecido/fallecidos/fallecidos.component';
 import { InicioComponent } from './inicio/inicio/inicio.component';
 import { HistorialComponent } from './inicio/historial/historial.component';
 import { RepresentanteComponent } from './inicio/representante/representante.component';
 import { RepresentantesComponent } from './cementerio/representante/representantes/representantes.component';
+import { SitioDetallesInformacionComponent } from './cementerio/sitio/sitio-detalles-informacion/sitio-detalles-informacion.component';
+import { SitioDetallesEstadoCuentaComponent } from './cementerio/sitio/sitio-detalles-estado-cuenta/sitio-detalles-estado-cuenta.component';
+import { SitioDetallesListaFallecidosComponent } from './cementerio/sitio/sitio-detalles-lista-fallecidos/sitio-detalles-lista-fallecidos.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
@@ -35,9 +36,14 @@ const routes: Routes = [
       {
         path: 'registro/:id', component: RepresentanteInformacionComponent,
         children: [
-          { path: 'sitios', component: SitiosComponent },
-          { path: 'abonos', component: PagosComponent },
-          { path: 'cargos', component: DeudasComponent },
+          {
+            path: 'sitios', component: SitiosComponent,
+            children: [
+              { path: 'informacion', component: SitioDetallesInformacionComponent },
+              { path: 'fallecidos', component: SitioDetallesListaFallecidosComponent },
+              { path: 'estado-cuenta', component: SitioDetallesEstadoCuentaComponent },
+            ]
+          },
           { path: 'estado-cuenta', component: EstadoCuentaComponent },
           { path: 'fallecidos', component: FallecidosComponent },
         ]
