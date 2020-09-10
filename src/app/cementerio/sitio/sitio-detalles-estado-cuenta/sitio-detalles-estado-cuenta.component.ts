6144a9e1b1fa6a9185b86dce715786cb9fa0649f
jssalazar <jssalazar3@espe.edu.ec>
2020-09-09 20:06:23 -0500

    CEMENTERIO SITIO DETALLES ESTADO CUENTA

--- src/app/cementerio/sitio/sitio-detalles-estado-cuenta/sitio-detalles-estado-cuenta.component.ts
+++ src/app/cementerio/sitio/sitio-detalles-estado-cuenta/sitio-detalles-estado-cuenta.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { tap } from 'rxjs/operators';
 import { ServiceC } from '../service-c/sitio-serviceC';
-import { SitioI, ResponseSitioI, ResponseEstadoCuentaSitioI, EstadoCuentaI } from '../model/sitio';
+import { ResponseEstadoCuentaSitioI, EstadoCuentaI } from '../model/sitio';
 import { SitioService } from '../service/sitio.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatAccordion } from '@angular/material/expansion';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-sitio-detalles-estado-cuenta',
@@ -16,22 +20,54 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 })
 export class SitioDetallesEstadoCuentaComponent implements OnInit {
 
+  @ViewChild('accordion_abono') accordion: MatAccordion;
+  @ViewChild('accordion_cargo') accordion_cargo: MatAccordion;
+  @ViewChild('accordion_editar') accordion_editar: MatAccordion;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  displayedColumnsEC: string[] = ['fecha', 'descripcion', 'desde',  'cargos', 'abonos', 'acciones'];
+  displayedColumnsEC: string[] = ['select', 'fecha', 'descripcion', 'cargos', 'abonos', 'acciones'];
   dataSource: MatTableDataSource<EstadoCuentaI>;
+  selection = new SelectionModel<EstadoCuentaI>(true, []);
 
   id_sitio: string = "";
   locale: string;
 
   listaEstadoCuenta: EstadoCuentaI[] = [];
 
+  agregarAbonoForm: FormGroup = this.fb.group({
+    sitio: '',
+    fecha: [new Date(), Validators.required],
+    descripcion: ['', Validators.required],
+    cantidad: ['', [Validators.required, Validators.min(0)]]
+  })
+
+  agregarCargoForm: FormGroup = this.fb.group({
+    sitio: '',
+    renocacion: 0,
+    desde: [new Date(), Validators.required],
+    descripcion: ['', Validators.required],
+    cantidad: ['', [Validators.required, Validators.min(0)]]
+  })
+
+  editarForm: FormGroup = this.fb.group({
+    id: '',
+    sitio: '',
+    renocacion: 0,
+    transaccion: '',
+    tipo: '',
+    fecha: [new Date(), Validators.required],
+    descripcion: ['', Validators.required],
+    cantidad: ['', [Validators.required, Validators.min(0)]]
+  })
+
   constructor(
     private translate: TranslateService,
     private route: ActivatedRoute,
     private sc: ServiceC,
-    private apiSitios: SitioService,) {
+    private _snackBar: MatSnackBar,
+    private apiSitio: SitioService,
+    private fb: FormBuilder,) {
     route.queryParamMap.pipe(
       tap((data: ParamMap) => {
         if (data.get("id")) {
@@ -46,53 +82,139 @@ export class SitioDetallesEstadoCuentaComponent implements OnInit {
     this.translate.onLangChange.pipe(
       tap((langChangeEvent: LangChangeEvent) => { this.locale = langChangeEvent.lang; })
     ).toPromise();
-
   }
 
-  getTotalEstadoCuenta(): number {
-    let suma: number = 0;
-    this.listaEstadoCuenta.forEach(t => {
-      if (new Date(t.fecha) > new Date('2001/01/01')) {
-        if (t.estado_cuenta === 'deuda') {
-          suma += Number(t.cantidad);
-        } else {
-          if(t.descripcion === "Servicio" || t.descripcion === "Mantenimiento") {
-            suma -= Number(t.cantidad);
+  get agregarAbonoFormControl() {
+    return this.agregarAbonoForm.controls;
+  }
+
+  get agregarCargoFormControl() {
+    return this.agregarCargoForm.controls;
+  }
+
+  get editarFormControl() {
+    return this.editarForm.controls;
+  }
+
+  submitAbono = (): void => {
+    if (this.agregarAbonoForm.valid) {
+      this.apiSitio.agregarPago([this.agregarAbonoForm.value])
+        .pipe(tap(data => {
+          if (data.ok) {
+            this.openSnackBar("Abono registrado!! ", "ok");
+            this.obtenerValores(this.id_sitio);
+          } else {
+            this.openSnackBar("A ocurrido un error, por favor inténtanlo nuevamente ", "ok");
           }
+          this.cancelarSubmitAbono();
+        })).toPromise();
+    }
+  }
+
+  submitCargo = (): void => {
+    if (this.agregarCargoForm.valid) {
+      this.apiSitio.agregarDeuda([this.agregarCargoForm.value])
+        .pipe(tap(data => {
+          if (data.ok) {
+            this.openSnackBar("Cargo registrado!! ", "ok");
+            this.obtenerValores(this.id_sitio);
+          } else {
+            this.openSnackBar("A ocurrido un error, por favor inténtanlo nuevamente ", "ok");
+          }
+          this.cancelarSubmitCargo();
+        })).toPromise();
+    }
+  }
+
+
+  eliminarLista = (): void => this.eliminar(this.selection.selected);
+
+  eliminarEstadoCuenta = (row: any): void => this.eliminar([row]);
+
+  editar = (row: EstadoCuentaI) => {
+    this.editarForm.patchValue(row);
+    this.editarForm.controls.tipo.setValue(row.estado_cuenta);
+    this.editarForm.controls.sitio.setValue(this.id_sitio);
+    this.accordion_editar.openAll();
+  }
+
+  submitEditar = (): void => {
+    if (this.editarForm.valid) {
+      this.apiSitio.actualizarSitioEstadoCuenta(this.editarForm.value)
+        .pipe(tap(data => {
+          if (data.ok) {
+            this.openSnackBar("Registro actualizado!! ", "ok");
+            this.obtenerValores(this.id_sitio);
+          } else {
+            this.openSnackBar("A ocurrido un error, por favor inténtanlo nuevamente ", "ok");
+          }
+          this.cancelarSubmitEditar();
+        })).toPromise();
+    }
+  }
+
+  cancelarSubmitAbono(): void {
+    this.accordion.closeAll();
+    this.agregarAbonoForm.reset();
+    this.agregarAbonoForm.controls.fecha.setValue(new Date());
+    this.agregarAbonoForm.controls.sitio.setValue(this.id_sitio);
+  }
+
+  cancelarSubmitCargo(): void {
+    this.accordion_cargo.closeAll();
+    this.agregarCargoForm.reset();
+    this.agregarCargoForm.controls.desde.setValue(new Date());
+    this.agregarCargoForm.controls.sitio.setValue(this.id_sitio);
+  }
+
+  cancelarSubmitEditar(): void {
+    this.accordion_editar.closeAll();
+  }
+
+  isAllSelected(): boolean {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle(): void {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  getDeudas = (): number => this.listaEstadoCuenta
+    .filter(t =>
+      t.estado_cuenta.toLowerCase() === 'deuda' &&
+      (new Date(t.fecha) > new Date('2001/01/01')))
+    .reduce((a, b) => a + Number(b.cantidad), 0);
+
+  getPagos = (): number => this.listaEstadoCuenta
+    .filter(t =>
+      t.estado_cuenta.toLowerCase() !== 'deuda' &&
+      (new Date(t.fecha) > new Date('2001/01/01')))
+    .reduce((a, b) => a + Number(b.cantidad), 0);
+
+  private eliminar(data): void {
+    this.apiSitio.eliminarEstadoCuenta(data).pipe(
+      tap((x: any) => {
+        if (x.ok) {
+          this.obtenerValores(this.id_sitio);
+          this.openSnackBar("Registros eliminados correctamente", "ok");
+        } else {
+          this.openSnackBar("A ocurrido un error, por favor inténtanlo nuevamente", "ok");
         }
-      }
-    });
-    return suma;
-  }
-
-  getDeudasEstadoCuenta(): number {
-    let suma: number = 0;
-    this.listaEstadoCuenta.forEach(t => { if (t.estado_cuenta === 'deuda' && (new Date(t.fecha) > new Date('2001/01/01'))) { suma += Number(t.cantidad); } });
-    return suma;
-  }
-
-  getPagos(): number {
-    let suma: number = 0;
-    this.listaEstadoCuenta.forEach(t => { if (t.estado_cuenta !== 'deuda' && (new Date(t.fecha) > new Date('2001/01/01')) ) { suma += Number(t.cantidad); } });
-    return suma;
-  }
-
-  getPagosServicios(): number {
-    let suma: number = 0;
-    this.listaEstadoCuenta.forEach(t => { if (t.estado_cuenta !== 'deuda' && (new Date(t.fecha) > new Date('2001/01/01')) && t.desde !== null ) { suma += Number(t.cantidad); } });
-    return suma;
-  }
-
-  getPagosExtras(): number {
-    let suma: number = 0;
-    this.listaEstadoCuenta.forEach(t => { if (t.estado_cuenta !== 'deuda' && t.desde === null) { suma += Number(t.cantidad); } });
-    return suma;
+      })
+    ).toPromise();
   }
 
   private obtenerValores(id_sitio: string) {
     this.id_sitio = id_sitio;
-    this.sc.emitIdSitioDetalleChange(Number(id_sitio));
-    this.apiSitios.obtenerEstadoCuenta(id_sitio).pipe(
+    this.selection.clear();
+    this.agregarAbonoForm.controls.sitio.setValue(this.id_sitio);
+    this.agregarCargoForm.controls.sitio.setValue(this.id_sitio);
+    this.sc.emitIdSitioDetalleChange(Number(this.id_sitio));
+    this.apiSitio.obtenerEstadoCuenta(this.id_sitio).pipe(
       tap((data: ResponseEstadoCuentaSitioI) => {
         if (data.ok) {
           this.cargarValoresEstadoCuenta(data.data);
@@ -103,7 +225,6 @@ export class SitioDetallesEstadoCuentaComponent implements OnInit {
   }
 
   private cargarValoresEstadoCuenta(data: EstadoCuentaI[]) {
-    console.log(data);
     this.listaEstadoCuenta = data.map((d: EstadoCuentaI) => {
       let nuevo: any = {};
       nuevo = d;
@@ -113,6 +234,10 @@ export class SitioDetallesEstadoCuentaComponent implements OnInit {
     this.dataSource = new MatTableDataSource(this.listaEstadoCuenta);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+  }
+
+  private openSnackBar = (message: string, action: string) => {
+    this._snackBar.open(message, action, { duration: 5000 });
   }
 
 }
