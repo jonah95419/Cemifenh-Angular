38993de0a9c3fe2457f0064d5b807774f2fa33b1
jssalazar <jssalazar3@espe.edu.ec>
2020-09-09 23:30:33 -0500

    CEMENTERIO REPRESENTANTE ESTADO DE CUENTA

--- src/app/cementerio/representante/estado-cuenta/estado-cuenta.component.ts
+++ src/app/cementerio/representante/estado-cuenta/estado-cuenta.component.ts
@@ -1,71 +1,201 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, ParamMap, Params } from '@angular/router';
 import { RepresentanteService } from '../service/representante.service';
 import { tap } from 'rxjs/operators';
-import { ResponseDeudaRepresentanteI, DeudaRepresentanteI } from '../model/deuda';
 import { ServiceC } from '../../sitio/service-c/sitio-serviceC';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE, DateAdapter } from '@angular/material/core';
+import { DialogEstadoCuenta } from '../../sitio/dialog/editar-estado-cuenta/editar-estado-cuenta';
+import { DialogRegistroDeuda } from '../dialog/registro-deuda/dialog-registro-deuda';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SitioService } from '../../sitio/service/sitio.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogPagoExtra } from '../dialog/registro-pago-extra/dialog-pago-extra';
+import { RepresentantesResponse, RepresentanteI } from '../model/representante';
+import { ResponseSitioI } from '../../sitio/model/sitio';
+import { EstadoCuentaH } from '../model/estadoCuentaR';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-estado-cuenta',
   templateUrl: './estado-cuenta.component.html',
-  styleUrls: ['./estado-cuenta.component.css']
+  styleUrls: ['./estado-cuenta.component.css'],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'es-EC' },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+  ],
 })
 export class EstadoCuentaComponent implements OnInit {
 
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  listaEstadoCuenta = [];
+  displayedColumnsEC: string[] = ['select', 'fecha', 'sector', 'descripcion', 'cargos', 'abonos', 'acciones'];
 
-  displayedColumns: string[] = ['id', 'nombre', 'sector', 'tipo', 'descripcion', 'pago', 'desde', 'hasta', 'cantidad'];
-  dataSource: MatTableDataSource<DeudaRepresentanteI>;
+  dataSourceEC: MatTableDataSource<EstadoCuentaH>;
+  selection = new SelectionModel<EstadoCuentaH>(true, []);
+
+  listaEstadoCuenta: EstadoCuentaH[] = [];
+  representante: RepresentanteI;
+
+  id: string;
+  locale: string;
+  fecha: Date = new Date();
+
+  private _translate;
 
   constructor(
-    private sc: ServiceC,
-    private route: ActivatedRoute,
+    private _snackBar: MatSnackBar,
+    private translate: TranslateService,
     private apiRepresentante: RepresentanteService,
-    ) {
-      route.parent.params.pipe( tap((data: Params) => {
-        if(data.id) {
-          this.obtenerValores(data.id);
+    private apiSitio: SitioService,
+    private notsitio: ServiceC,
+    private dialog: MatDialog,
+    private route: ActivatedRoute) {
+    route.parent.params.pipe( tap((data: Params) => {
+      if(data.id) {
+        const representante = data.id;
+        this.id = representante;
+        this.obtenerHistorial();
+      }
+    })).toPromise();
+    notsitio.actualizarHistorial$.pipe(
+      tap((x: any) => this.obtenerHistorial())
+    ).toPromise();
+  }
+
+  ngOnInit() {
+    this.locale = this.translate.currentLang;
+    this._translate = this.translate.onLangChange.pipe(
+      tap((langChangeEvent: LangChangeEvent) => { this.locale = langChangeEvent.lang; })
+    ).toPromise();
+  }
+
+  isAllSelected(): boolean {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSourceEC.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle(): void {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSourceEC.data.forEach(row => this.selection.select(row));
+  }
+
+  imprimirLista = (): void => {
+
+  }
+
+  imprimir = (): void => {
+
+  }
+
+  editar = (row: any) => {
+    row.pago = String(row.pago).toLowerCase();
+    const dialogRef = this.dialog.open(DialogEstadoCuenta,{ width: '350px', panelClass: "my-class", data: row });
+    dialogRef.afterClosed().subscribe();
+  }
+
+  eliminarLista = (): void => this.eliminar(this.selection.selected);
+
+  eliminarEstadoCuenta = (row: any): void => this.eliminar([row]);
+
+  agregarDeuda(): void {
+    const dialogRef = this.dialog.open(DialogRegistroDeuda, {
+      width: '500px',
+      panelClass: "my-class",
+      data: { id: this.id }
+    });
+
+    dialogRef.afterClosed().subscribe();
+  }
+
+  agregarPago(): void {
+    const dialogRef = this.dialog.open(DialogPagoExtra, {
+      width: '500px',
+      panelClass: "my-class",
+      data: { id: this.id }
+    });
+
+    dialogRef.afterClosed().subscribe();
+  }
+
+  getDeudas = (): number => this.listaEstadoCuenta
+    .filter(t =>
+      t.estado_cuenta.toLowerCase() === 'deuda' &&
+      (new Date(t.fecha) > new Date('2001/01/01')))
+    .reduce((a, b) => a + Number(b.cantidad), 0);
+
+  getPagos = (): number => this.listaEstadoCuenta
+    .filter(t =>
+      t.estado_cuenta.toLowerCase() !== 'deuda' &&
+      (new Date(t.fecha) > new Date('2001/01/01')))
+    .reduce((a, b) => a + Number(b.cantidad), 0);
+
+
+  private eliminar(data): void {
+    this.apiSitio.eliminarEstadoCuenta(data).pipe(
+      tap((x: any) => {
+        if (x.ok) {
+          this.obtenerHistorial();
+          this.openSnackBar("Registros eliminados correctamente", "ok");
+        } else {
+          this.openSnackBar("A ocurrido un error, por favor inténtanlo nuevamente", "ok");
         }
-      })).toPromise();
-     }
-
-  ngOnInit() { }
-
-  getTotalEstadoCuenta(): number {
-    let suma = 0;
-    this.listaEstadoCuenta.forEach( t => { if(new Date(t.desde) > new Date('2001/01/01') ) {if ( t.estado_cuenta === 'deuda' ) { suma += Number(t.cantidad); } else { suma -= Number(t.cantidad); }}});
-    return suma;
-  }
-
-  getDeudasEstadoCuenta(): number {
-    let suma = 0;
-    this.listaEstadoCuenta.forEach( t => { if ( t.estado_cuenta === 'deuda' && (new Date(t.desde) > new Date('2001/01/01'))) { suma += Number(t.cantidad); }});
-    return suma;
-  }
-
-  getPagosEstadoCuenta(): number {
-    let suma = 0;
-    this.listaEstadoCuenta.forEach( t => { if ( t.estado_cuenta !== 'deuda' && (new Date(t.desde) > new Date('2001/01/01'))) { suma += Number(t.cantidad); }});
-    return suma;
-  }
-
-  private obtenerValores(id: string): void {
-    this.sc.emitIdSitioDetalleChange(null);
-    this.apiRepresentante.obtenerEstadoCuentaRepresentante(id).pipe(
-      tap((data: ResponseDeudaRepresentanteI) => {
-        if (data.ok) { this.cargarValores(data.data); } else {alert(data.message); }
       })
     ).toPromise();
   }
 
-  private cargarValores(data: DeudaRepresentanteI[]): void {
-    this.listaEstadoCuenta = data;
-    this.dataSource = new MatTableDataSource(this.listaEstadoCuenta);
-    this.dataSource.sort = this.sort;
+  private obtenerHistorial(): void {
+    if (!this.id) return;
+    this.notsitio.emitIdSitioDetalleChange(null);
+    this.selection.clear();
+    this.cargarHistorial(this.id);
+    this.cargarRepresentante(this.id);
+  }
+
+  private cargarRepresentante(id: string): void {
+    this.apiRepresentante.obtenerRepresentante(id).pipe(
+      tap((data: RepresentantesResponse) => {
+        if (data.ok) { this.representante = data.data[0]; }
+        else { console.log(data.message); }
+      })
+    ).toPromise();
+  }
+
+  private cargarHistorial(id: string): void {
+    this.apiRepresentante.obtenerEstadoCuentaRepresentante(id).pipe(
+      tap((data: any) => {
+        if (data.ok) { this.cargarValoresEstadoCuenta(data.data); }
+        else { console.log(data.message); }
+      })
+    ).toPromise();
+  }
+
+  private cargarValoresEstadoCuenta(data: EstadoCuentaH[]): void {
+    this.listaEstadoCuenta = data.map((value: EstadoCuentaH) => {
+      let nuevo: any = value;
+      nuevo.hovered = false;
+      nuevo.editar = false;
+      return nuevo;
+    });
+    this.dataSourceEC = new MatTableDataSource(this.listaEstadoCuenta);
+    this.dataSourceEC.paginator = this.paginator;
+    this.dataSourceEC.sort = this.sort;
+  }
+
+  private openSnackBar = (message: string, action: string) => {
+    this._snackBar.open(message, action, { duration: 5000 });
   }
 
 }
