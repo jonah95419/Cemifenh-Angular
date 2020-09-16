4124d24c2a41f40590bc9681be9a1a41cf8fbfed
jssalazar <jssalazar3@espe.edu.ec>
2020-09-16 15:02:15 -0500

    USER MODULE

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AngularModule } from '../angular.module';
+import { MatInputModule } from '@angular/material/input';
+import { CallbackComponent } from '../callback/callback.component';
+
+@NgModule({
+  declarations: [
+    IngresoComponent,
+    CallbackComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    CommonModule,
+    AngularModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
+  ],
+  exports: [
+    AngularModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+
+})
+
+export class UserModule { }
