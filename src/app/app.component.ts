b808c169227d2ba4303b42fab78d13091a2dd1bc
jssalazar <jssalazar3@espe.edu.ec>
2020-09-16 14:59:06 -0500

    APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { SitioService } from './cementerio/sitio/service/sitio.service';
 import { RepresentanteService } from './cementerio/representante/service/representante.service';
 import { SectorService } from './admin/service/sector.service';
 import { ValoresService } from './admin/service/valores.service';
+import { AuthenticationService } from './core/service/authentication.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +15,24 @@ export class AppComponent {
   title = 'cementerio';
 
   constructor(
+    public authenticationService: AuthenticationService,
     private apiSitios: SitioService,
     private apiValores: ValoresService,
     private apiRepresentantes: RepresentanteService,
     private apiSectores: SectorService) {
-    apiSitios.listarFechasSitios();
-    apiSectores.listarSectores();
-    apiValores.listarValores();
-    apiValores.listarValorPagoExtra();
-    apiRepresentantes.listarRepresentantesTodo();
+    authenticationService.user$.pipe(
+      tap((x: any) => {
+        const accessToken = localStorage.getItem('access_token');
+        if (x && accessToken) {
+          apiSitios.listarFechasSitios();
+          apiSectores.listarSectores();
+          apiValores.listarValores();
+          apiValores.listarValorPagoExtra();
+          apiRepresentantes.listarRepresentantesTodo();
+        }
+      })
+    ).toPromise();
+
   }
 
 }
