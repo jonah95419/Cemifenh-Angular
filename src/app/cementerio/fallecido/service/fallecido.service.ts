7c204021c9cfc45d1f0b3743d19a3f6927407fb4
jssalazar <jssalazar3@espe.edu.ec>
2020-09-16 15:01:01 -0500

    CEMENTERIO FALLECIDO SERVICE

--- src/app/cementerio/fallecido/service/fallecido.service.ts
+++ src/app/cementerio/fallecido/service/fallecido.service.ts
@@ -51,7 +51,7 @@ export class FallecidoService {
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type':  'application/json',
-      // 'authorization': `Bearer ${JSON.parse(localStorage.getItem('user')).stsTokenManager.accessToken}`
+      'authorization': `Bearer ${localStorage.getItem('access_token')}`
     })
   };
 }
