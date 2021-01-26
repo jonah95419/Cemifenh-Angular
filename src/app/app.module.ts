892993b850ada355b7e109403cd00f47ae1a8c9e
jssalazar <jonah95419@gmail.com>
2021-01-26 09:52:16 -0500

    HASH - app routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CabeceraComponent } from './cabecera/cabecera.component';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
-import { registerLocaleData } from '@angular/common';
+import { LocationStrategy, PathLocationStrategy, registerLocaleData } from '@angular/common';
 import localeES from '@angular/common/locales/es-EC';
-import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {HttpClient} from '@angular/common/http';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
 
-import { environment } from '../environments/environment';
 import { FallecidoModule } from './cementerio/fallecido/fallecido.module';
 import { RepresentanteModule } from './cementerio/representante/representante.module';
 import { SitioModule } from './cementerio/sitio/sitio.module';
-import { AdminModule } from './admin/admin.module';
 import { InicioModule } from './inicio/inicio.module';
 import { ReportesModule } from './reportes/reportes.module';
 import { UserModule } from './user/user.module';
 import { CoreModule } from './core/core.module';
 
 registerLocaleData(localeES, 'es');
-const AUTH_SERVER = environment.baseUrl
 
 @NgModule({
   declarations: [
@@ -41,30 +38,32 @@ const AUTH_SERVER = environment.baseUrl
     FallecidoModule,
     RepresentanteModule,
     SitioModule,
-    // AdminModule.forRoot(),
     InicioModule,
     ReportesModule,
     UserModule,
     CoreModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient]
-            }
-        }),
+    // AdminModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: PathLocationStrategy }
+  ],
   bootstrap: [AppComponent],
 
 })
 export class AppModule {
-  constructor(private translate: TranslateService){
+  constructor(private translate: TranslateService) {
     translate.addLangs(['es']);
     translate.setDefaultLang('es');
     translate.use('es');
- }
- }
+  }
+}
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
